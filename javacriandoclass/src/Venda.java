3ab0ee9afc297d70f1f5cac29eed36848d6c0493
GustavoMelo1 <gustavomelo.oliveira@hotmail.com>
2025-09-27 18:19:22 -0300

    Salvando

--- javacriandoclass/src/Venda.java
+++ javacriandoclass/src/Venda.java
@@ -1,2 +1,8 @@
-public class Venda {
+// Public : serve para que todas as classes tenham acesso ao metodo , no caso compartilhar com o resto do codigo . //
+
+// Void : serve para nao dar retorno nenhum, no caso vai executar algo no metodo porem nao vai dar retorno nele . //
+
+// Class : serve para declarar a variavel . //
+
+ class Venda {
 }
